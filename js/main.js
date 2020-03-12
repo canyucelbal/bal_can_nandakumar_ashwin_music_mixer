3d9405079d046b559d6a71e56d5f2f36dc8ea2f2
ashwin <ashu61998@gmail.com>
2020-03-12 09:56:07 -0400

    Updated js

--- js/main.js
+++ js/main.js
@@ -5,12 +5,26 @@
 
 	musicIcons = document.querySelector('.musicIconsArea');
 
+const pieceNames = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16"];
 
+function getref(){
+	pieceNames.forEach((piece, index) =>{
+	iconrow[index].src = `images/icon_jpg_files/${"music_icons" + piece}.svg`;
+	iconrow[index].id = `${"music_icons" + piece}`;
+});
+}
+
+function resetPieces(){
+	for (let i=0; i < dropZones.length; i++){
+		if(dropZones[i].childNodes.length != 0)
+		musicIcons.appendChild(dropZones[i].firstChild);
+	}
+}
 
 function allowDrag(event) {
 	console.log ('dragged an image');
 
-event.dataTransfer.setData("text/plain", dropZones.id);
+event.dataTransfer.setData("text/plain", this.id);
 }
 
 
@@ -20,12 +34,13 @@ function allowDragOver(event) {
 }
 
 function allowDrop(event) {
+	getref();
 	console.log("drop");
-  let draggedImage = event.dataTransfer.getData("text/plain", dropZones.id);
+  let draggedImage = event.dataTransfer.getData("text/plain", this.id);
 
- if (droparea.childNodes.length == 0){
+ if (this.childNodes.length == 0){
 
- 	dropZones.appendChild(document.querySelector(`#${draggedImage}`));
+ 	this.appendChild(document.querySelector(`#${draggedImage}`));
  }
  else if (this.childNodes.length > 0){
  	this.appendChild(document.querySelector(`#${draggedImage}`));
@@ -34,7 +49,9 @@ function allowDrop(event) {
  }
  }
 
+iconrow.forEach(piece => piece.addEventListener('click', getref));
 
+iconrow.forEach(piece => piece.addEventListener('click', resetPieces))
 
 iconrow.forEach(piece => piece.addEventListener('dragstart', allowDrag));
 
