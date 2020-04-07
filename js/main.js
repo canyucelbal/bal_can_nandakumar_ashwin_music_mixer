0b7503a9cfa9ac478157fdb051e878077a77e1b2
ashwin <ashu61998@gmail.com>
2020-04-07 12:42:42 -0400

    Js and html updated

--- js/main.js
+++ js/main.js
@@ -3,8 +3,12 @@
 	dropZones = document.querySelectorAll('.dropArea'),
 	iconrow = document.querySelectorAll('.IconRow img'),
 	audioElement = document.querySelector('audio'),
-	musicIcons = document.querySelector('.musicIconsArea');
-	
+	musicIcons = document.querySelector('.musicIconsArea'),
+	pause = document.querySelector('.pause'),
+	play = document.querySelector('.play'),
+	refresh = document.querySelector('.refresh');
+
+let globalPaused = false;	
 
 const pieceNames = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16"];
 
@@ -12,7 +16,12 @@ function playTrack(track) {
  
 console.log("playing audio");
   audioElement.src = `audio/${track}.wav`;
+  if (globalPaused) {
+    console.log('paused');
+    resumeTrack();
+    return;
 
+  }
  audioElement.load();
 audioElement.play();
 
@@ -63,6 +72,22 @@ function allowDrop(event) {
 playTrack(trackref);
 }
 
+
+
+function pauseTrack() {
+  audioElement.pause();
+  globalPaused = true;
+}
+
+function playMusic() {
+  audioElement.play();
+}
+
+function refreshTrack() {
+audioElement.currentTime = 0;
+}
+
+
 iconrow.forEach(piece => piece.addEventListener('click', getref));
 
 iconrow.forEach(piece => piece.addEventListener('click', resetPieces));
@@ -72,5 +97,7 @@ iconrow.forEach(piece => piece.addEventListener('dragstart', allowDrag));
 dropZones.forEach(zone => zone.addEventListener('dragover', allowDragOver));
 
 dropZones.forEach(zone => zone.addEventListener('drop', allowDrop));	
-
+pause.addEventListener('click', pauseTrack);	
+play.addEventListener('click', playMusic);	
+refresh.addEventListener('click',refreshTrack);
 })();
